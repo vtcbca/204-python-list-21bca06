25d0ecb0d375d5d96e4c6cfdce540303473c564f
21bca06 <96610663+21bca06@users.noreply.github.com>
2022-04-21 15:54:35 +0530

    solution6 uploaded

---
+++ Solution/SOL6.py
@@ -0,0 +1,19 @@
+# WAS to enter any number from 1 to 7 and print appropiate day name.
+
+n=int(input("Enter any number:"))
+if(n==1):
+    print("SUNday")
+elif(n==2):
+    print("Monday")
+elif(n==3):
+    print("Tuesday")
+elif(n==4):
+    print("Wedesday")
+elif(n==5):
+    print("Thrusday")
+elif(n==6):
+    print("Friday")
+elif(n==7):
+    print("Saturday")
+else:
+    print("Invalid number")
