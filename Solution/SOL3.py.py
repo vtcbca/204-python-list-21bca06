cd890917d32ddf424aefac338e83d3015f195e33
21bca06 <96610663+21bca06@users.noreply.github.com>
2022-04-21 15:38:24 +0530

    Add files via upload
    Solution3 uploaded

---
+++ Solution/SOL3.py.py
@@ -0,0 +1,7 @@
+# WAS to enter number and check it is even number or not.
+
+n=int(input("Enter any number:"))
+if(n%2==0):
+    print("{} number is even number".format(n))
+else:
+    print("{} number is odd number".format(n))
